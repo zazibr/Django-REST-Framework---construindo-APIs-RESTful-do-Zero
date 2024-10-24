0aaaacd6eba23330e44d42f7383b7c25fecfb2fd
zazibr <daniel@lopes.ms>
2024-10-24 11:13:01 -0400

    com validações antes do almoço

--- escola/serializers.py
+++ escola/serializers.py
@@ -6,6 +6,20 @@ class EstudanteSerializer(serializers.ModelSerializer):
         model = Estudante
         fields = ['id', 'nome', 'email', 'cpf', 'data_nascimento', 'celular']
 
+    def validate_cpf(self, cpf):
+        if len( cpf ) != 11:
+            raise serializers.ValidationError('O CPF deve ter 11 dígitos!')        
+        return cpf
+    def validate_nome(self,nome):
+        if not nome.isalpha():
+            raise serializers.ValidationError('O NOME só pode ter letras')
+        return nome
+    def validate_celular(self,celular):
+        if len( celular ) != 13:
+            raise serializers.ValidationError('O CELULAR precisa ter 13 dígitos!')        
+        return celular
+
+
 class CursoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Curso
